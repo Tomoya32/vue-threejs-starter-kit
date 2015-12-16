8d8e3a3be5ccfca582da2ed2d0627363a0b54a36
Tomoya32 <tomoyamiyake32@gmail.com>
2015-12-16 13:02:32 +0100

    remove _gulp

--- _gulp/tasks/default.js
+++
@@ -1,8 +0,0 @@
-var gulp   = require('gulp');
-var config = require('../config');
-
-gulp.task('compile', ['sass', 'browserify:dev']);
-
-gulp.task('server', ['browserSync:server', 'compile']);
-
-gulp.task('default', ['watch']);
--- _gulp/tasks/watch.js
+++
@@ -1,7 +0,0 @@
-var gulp   = require('gulp');
-var config = require('../config');
-
-gulp.task('watch', ['browserSync:standalone', 'compile'], function(){
-  gulp.watch(config.styles.src, ['sass']);
-  gulp.watch(config.watch.files, ['compile']);
-});
